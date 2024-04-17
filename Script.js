d18e167c679c87dcea4fc3def3c76b0a8142a457
NPC-MARIMO <165462791+NPC-MARIMO@users.noreply.github.com>
2024-04-17 20:22:12 +0530

    Script.js

--- Script.js
+++ Script.js
@@ -1,38 +1,3 @@
-let scrollTriggerPlusLocomotiveJsCode = ()=>{
-    gsap.registerPlugin(ScrollTrigger);
-
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
-
-// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy("#main", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-});
-
-
-
-
-
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
-}
-// locomotive js is used for smooth scrolling but locomotive actually highjacks the scroll thing , so scrollTrigger does not work , the above code let locomotive and scrollTrigger work togather
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -406,5 +371,3 @@ page6Fn();
 page7Fn();
 
 page9Fn();
-
-scrollTriggerPlusLocomotiveJsCode();
