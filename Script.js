9fd6bc160364cbb349e20fffa7f191261dc8eeab
NPC-MARIMO <165462791+NPC-MARIMO@users.noreply.github.com>
2024-04-19 16:13:19 +0530

    Script.js

--- Script.js
+++ Script.js
@@ -1,5 +1,38 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+
+let main = document.querySelector("#main");
 
 gsap.registerPlugin(ScrollTrigger);
+
 
 let navFn = () => {
     let navs = document.querySelectorAll(".elem h4");
@@ -33,6 +66,7 @@ let navFn = () => {
         y: -90,
         scrollTrigger: {
             trigger: "#page2",
+            scroller: main,
             scrub: 1,
             start: "top bottom"
         }
@@ -41,6 +75,7 @@ let navFn = () => {
         y: -50,
         scrollTrigger: {
             trigger: "#page2",
+            scroller: main,
             scrub: 1,
             start: "top bottom"
         }
@@ -88,6 +123,7 @@ let page2RelemsFn = () => {
             scrub: 1,
             stagger: 0.3,
             scrollTrigger: {
+                scroller: main,
                 trigger: elem,
                 start: "top 85%",
                 toggleActions: "play none reverse reset"
@@ -98,6 +134,7 @@ let page2RelemsFn = () => {
             opacity: 0,
             scrub: 1,
             scrollTrigger: {
+                scroller: main,
                 trigger: "#p2LH5",
                 start: "top 90%",
                 toggleActions: "play none reverse reset"
@@ -243,6 +280,7 @@ let page4Fn = () => {
         opacity: 0,
         scrollTrigger: {
             trigger: "#page4L",
+            scroller: main,
             start: "top 80%",
             toggleActions: "play none reverse reset",
             scrub: true
@@ -253,6 +291,7 @@ let page4Fn = () => {
         opacity: 0,
         scrollTrigger: {
             trigger: "#page4R",
+            scroller: main,
             start: "top 80%",
             toggleActions: "play none none reset",
             scrub: true
@@ -282,6 +321,7 @@ let page6Fn = () => {
         y: 100,
         scrollTrigger: {
             trigger: "#page6Txt",
+            scroller: main,
             start: "center 90%",
             end: "center 65%",
             toggleActions: "play none none reset",
@@ -298,6 +338,7 @@ let page7Fn = () => {
             opacity: .4,
             scrollTrigger: {
                 trigger: left,
+                scroller: main,
                 start: "top 85%",
                 toggleActions: "play none none reset",
                 scrub: true,
@@ -314,6 +355,7 @@ let page7Fn = () => {
                 trigger: right,
                 start: "top 85%",
                 toggleActions: "play none none reset",
+                scroller: main,
                 end: "center 65%",
                 scrub: true,
             }
@@ -321,30 +363,34 @@ let page7Fn = () => {
 
     })
 }
-let page9Fn = ()=>{
-    gsap.to("#page9" , {
-        height : "68vh",
-        transition :"linear",
+let page9Fn = () => {
+    gsap.to("#page9", {
+        height: "68vh",
+        transition: "linear",
         scrollTrigger: {
-            trigger : "#page9",
-            start : "top 60%",
-            toggleActions : "play none none reset",
-            scrub : .5,
-            end : "top 35%",
+            trigger: "#page9",
+            start: "top 60%",
+            toggleActions: "play none none reset",
+            scroller: main,
+            scrub: .5,
+            end: "top 35%",
         }
     })
 }
-gsap.from(".page8Div" , {
-    opacity : 0 ,
-    y : 300,
-    stagger : .1,
-    scrollTrigger : {
-        trigger: ".page8Div",
-        start: "top 88%",
-        end : "top 50%",
-        toggleActions : "play none none reset"
-    }
-})
+let page8Fn = () => {
+    gsap.from(".page8Div", {
+        opacity: 0,
+        y: 300,
+        stagger: .1,
+        scrollTrigger: {
+            trigger: ".page8Div",
+            start: "top 88%",
+            end: "top 50%",
+            scroller: main,
+            toggleActions: "play none none reset"
+        }
+    })
+}
 
 navFn();
 
@@ -369,3 +415,5 @@ page6Fn();
 page7Fn();
 
 page9Fn();
+
+page8Fn();
